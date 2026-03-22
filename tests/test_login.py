286a9586c2f437308ca0b140e84bcfa3c8f7c0ae
Thuong212 <dothuong190401@gmail.com>
2026-03-22 09:29:05 +0700

    Login_function_completed

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,63 +1,23 @@
+import csv
+import pytest
 from pages.login_page import LoginPage
 
-def test_successful_login(driver):
-    # Arrange
+def read_test_data():
+    with open("login_user.csv") as f:
+        reader = csv.DictReader(f)
+        return [row for row in reader]
+
+@pytest.mark.parametrize("data", read_test_data())
+def test_login(driver, data):
     login_page = LoginPage(driver)
-
-    # Act
     login_page.open()
-    login_page.login("Admin", "admin123")
+    login_page.login(data["username"], data["password"])
 
-    # Assert
-    assert "orangehrm" in login_page.get_title().lower()
-
-
-def test_wrong_password_login(driver):
-    login_page = LoginPage(driver)
-
-    login_page.open()
-    login_page.login("Admin", "admin1234")
-
-    assert "Invalid credentials" in login_page.get_error_message()
-
-
-def test_wrong_username_login(driver):
-    login_page = LoginPage(driver)
-
-    login_page.open()
-    login_page.login("Admin1", "admin123")
-
-    assert "Invalid credentials" in login_page.get_error_message()
-
-
-def test_empty_username_login(driver):
-    login_page = LoginPage(driver)
-
-    login_page.open()
-    login_page.login("", "admin123")
-
-    msg = login_page.get_required_message()
-    assert msg in ["Requis", "Required"]
-
-
-def test_empty_password_login(driver):
-    login_page = LoginPage(driver)
-
-    login_page.open()
-    login_page.login("Admin", "")
-
-    msg = login_page.get_required_message()
-    assert msg in ["Requis", "Required"]
-
-
-def test_empty_username_password_login(driver):
-    login_page = LoginPage(driver)
-
-    login_page.open()
-    login_page.login("", "")
-
-    list_error_messages = login_page.get_all_required_messages()
-
-    assert len(list_error_messages) == 2
-    assert list_error_messages[0] in ["Requis", "Required"]
-    assert list_error_messages[1] in ["Requis", "Required"]
+    if data["expected"] == "success":
+        assert "orangehrm" in login_page.get_title().lower()
+    elif ";" in data["expected"]:  # trường hợp cả username và password rỗng
+        errors = login_page.get_all_required_messages()
+        for msg in errors:
+            assert msg in ["Required", "Requis"]
+    else:
+        assert data["expected"] in login_page.get_error_message()
